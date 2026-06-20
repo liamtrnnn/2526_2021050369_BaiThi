75ed43e8c7ef9d3694e48cf32d820a86e18eef78
“tranduythanhlam157@gmail.com” <tranduythanhlam157@gmail.com>
2026-06-20 16:02:54 +0700

    cau2

---
+++ Models/nhacungcap.cs
@@ -0,0 +1,18 @@
+using ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace 2526_2021050369_BaiThi.Models
+
+
+{
+    public class NhaCungCap
+
+ {
+    public string MaNCC { get; set; } = " ";
+
+    public string TenNCC { get; set; } = " ";
+
+
+
+
+}
+}
--- obj/Debug/net10.0/2526_2021050369_BaiThi.AssemblyInfo.cs
+++ obj/Debug/net10.0/2526_2021050369_BaiThi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2526_2021050369_BaiThi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f93864730cf5448b30d20977c053c151d066a30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a24e692fa0a72061337642c92ba9421443816ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("2526_2021050369_BaiThi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2526_2021050369_BaiThi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
